8e39365b84fa25cecadb70d9914c40e8a6ea5180
FabioVRocha <fabiovascrocha@gmail.com>
2025-07-19 10:35:08 -0300

    Exclui Sistema Antigo!

--- scripts/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Backup PostgreSQL database
-db_name="sigimob"
-backup_dir="${1:-$HOME}"
-file_name="sigimob_$(date +%Y%m%d_%H%M%S).sql"
-
-pg_dump "$db_name" > "$backup_dir/$file_name"
-
-echo "Backup salvo em $backup_dir/$file_name"
